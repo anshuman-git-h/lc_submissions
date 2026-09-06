79d0f5c988460a0b1d6b5cad5d2a08a616aa8f9a
anshuman-git-h <149307279+anshuman-git-h@users.noreply.github.com>
2026-09-06 12:54:04 +0530

    Time: 23 ms (77.66%), Space: 13 MB (79.71%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     int dp[1001][1001];
-
-    int fun(string &s, string &t, int i, int j)
-    {
-        if (j == t.size())
-            return 1;
-
-        if (i == s.size())
-            return 0;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        int ans = 0;
-
-        if (s[i] == t[j])
-        {
-            ans = fun(s, t, i + 1, j) 
-                + fun(s, t, i + 1, j + 1);
-        }
-        else
-        {
-            ans = fun(s, t, i + 1, j);
-        }
-
-        return dp[i][j] = ans;
-    }
-
+    const int md = 1e9+7;
     int numDistinct(string s, string t) {
-        memset(dp, -1, sizeof(dp));
-        return fun(s, t, 0, 0);
+        memset(dp,-1,sizeof(dp));
+        int n = s.size();
+        int m = t.size();
+        for(int i = 0;i<=m;i++)dp[n][i] = 0;
+        for(int i = 0;i<=n;i++)dp[i][m] = 1;
+
+        for(int i = n-1;i>=0;i--)
+        {
+            for(int j = m-1;j>=0;j--)
+            {
+                if(s[i] == t[j])
+                {
+                    dp[i][j] = (dp[i+1][j] + dp[i+1][j+1])%md;
+                }
+                else
+                {
+                    dp[i][j] = dp[i+1][j];
+                }
+            }
+        }
+
+        return dp[0][0];
     }
 };
