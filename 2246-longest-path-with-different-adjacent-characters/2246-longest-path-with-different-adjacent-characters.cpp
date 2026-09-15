6d951c193bab8927824b263bbad930d5653af2d0
anshuman-git-h <149307279+anshuman-git-h@users.noreply.github.com>
2026-09-16 00:30:31 +0530

    Time: 1912 ms (8.48%), Space: 213.6 MB (27%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,64 +1,43 @@
 class Solution {
 public:
+    int ans = 0;
+    string ss;
     vector<vector<int>> adj;
-    pair<int,int> fun(int node,int par)
+    int fun(int node,int par)
     {
-        pair<int,int> ans = {node,0};
+        int mx = 0;
+        int smx = 0;
         for(auto it: adj[node])
         {
             if(it == par)continue;
+            int chl = fun(it,node);
+            if(ss[it] == ss[node])continue;
 
-            pair<int,int> tmp = fun(it,node);
-            if(tmp.second + 1 > ans.second)
+            if(chl > mx)
             {
-                ans = {tmp.first,tmp.second + 1};
+                smx = mx;
+                mx = chl;
+            }
+
+            else if(chl > smx)
+            {
+                smx = chl;
             }
         }
-        return ans;
-    }
-    int dia(int node)
-    {
-        pair<int,int> p1 = fun(node,-1); //node, dia
-        pair<int,int> p2 = fun(p1.first,-1);
-        return p2.second + 1;
+        ans = max({ans, 1 + mx + smx, 1 + mx});
+        return 1 + mx;
+
     }
     int longestPath(vector<int>& parent, string s) {
-        //diameter on the the connected components after removing edges with the same chars
-        int n = parent.size();
+        int n = s.size();
         adj.resize(n,{});
-
         for(int i = 1;i<n;i++)
         {
-            int par = parent[i];
-
-            if(s[par] == s[i])continue;
-            adj[par].push_back(i);
-            adj[i].push_back(par);
+            adj[i].push_back(parent[i]);
+             adj[parent[i]].push_back(i);
         }
-        vector<int> vis(n,0);
-        int ans = 1;
-        for(int i = 0;i<n;i++)
-        {
-            if(vis[i])continue;
-
-            queue<int> q;
-            q.push(i);
-            while(!q.empty())
-            {
-                int node = q.front();
-                q.pop();
-                for(auto it: adj[node])
-                {
-                    if(vis[it])continue;
-                    vis[it] = 1;
-                    q.push(it);
-                }
-            }
-
-            ans = max(ans, dia(i));
-        }
+        ss = s;
+        int a = fun(0,-1);
         return ans;
-
-
     }
 };
