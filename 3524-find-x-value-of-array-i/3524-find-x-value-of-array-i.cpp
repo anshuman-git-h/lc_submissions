d84baadea5cabf7aea5c35717cb0d79bee322ed6
anshuman-git-h <149307279+anshuman-git-h@users.noreply.github.com>
2026-09-21 23:21:27 +0530

    Time: 174 ms (29.46%), Space: 164.7 MB (33.93%) - LeetHub

---
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<long long> resultArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<vector<int>> ans(n,vector<int>(k,0));
+        
+        for(int i = 0;i<n;i++)
+        {
+            nums[i] = nums[i]%k;
+           // if(i)nums[i] = (nums[i]*nums[i-1]) % k;
+        }
+
+        for(int i = 0;i<n;i++)
+        {
+            ans[i][nums[i] % k]++;
+            if(i == 0)continue;
+            for(int j = 0;j<k;j++)
+            {
+                ans[i][(j*nums[i])%k] += ans[i-1][j];
+            }
+        }
+        vector<long long> res;
+        for(int i = 0;i<k;i++)
+        {
+            long long sm = 0;
+            for(int j= 0;j<n;j++)sm+=ans[j][i];
+            res.push_back(sm);
+        }
+        return res;
+    }
+};
+
+
