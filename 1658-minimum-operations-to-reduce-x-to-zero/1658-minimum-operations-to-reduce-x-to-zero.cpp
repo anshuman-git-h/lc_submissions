3cfb3b6b7588285f6ac4aa1865528bb099de5c33
anshuman-git-h <149307279+anshuman-git-h@users.noreply.github.com>
2026-09-23 19:19:45 +0530

    Time: 0 ms (100%), Space: 102.3 MB (65.24%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        unordered_map<int,int> mp;//suffix sum and index
-        int sm = 0;
         int n = nums.size();
-        bool ok = false;
-        mp[0] = n;
-        for(int i = n-1;i>=0;i--){
-            sm += nums[i];
-            mp[sm] = i;
-        }
-        
-        int ans = n;
-        if(mp.find(x)!=mp.end()){
-            ok = true;
-            ans = n - mp[x];
-        }
-        int pre = 0;
-        
-        for(int i = 0;i<n;i++){
-            pre+=nums[i];
-            if(mp.find(x - pre)!=mp.end()){
-                int r = mp[x - pre];
-                if(r <= i)continue;
-                ok = true;
-                
-                r = n - r;
-                if(r + i + 1 < ans){
-                    ans = r + i + 1;
-                }
+        int sum = accumulate(nums.begin(),nums.end(),0);
+
+        int tar = sum - x;
+        if(tar == 0)return n;
+        int j = 0;
+        int i = 0;
+        int len = 0;
+        int sm = 0;
+        while(j<n)
+        {
+            sm += nums[j];
+            while(sm>tar && i<n){
+                sm-=nums[i++];
             }
+
+            if(sm == tar){
+                len = max(len, j - i + 1);
+            }
+            j++;
         }
-        if(ok)return ans;
-        return -1;
+        int res = n - len;
+        if(res == n)res = -1;
+        return res;
+
     }
 };
